8696342cb0a863068382f41e99a3d83c13dbe6c3
Nitin1438 <nitin.chobhe@gmail.com>
2018-08-08 15:30:54 +0000

    Remove NFC for now

--- device.mk
+++ device.mk
@@ -141,18 +141,18 @@ PRODUCT_COPY_FILES += \
 	frameworks/native/data/etc/android.hardware.ethernet.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.ethernet.xml
 
 # NFC packages
-PRODUCT_PACKAGES += \
-    nfc_nci.bcm2079x.default \
-    android.hardware.nfc@1.0-impl \
-    android.hardware.nfc@1.0-service \
-    NfcNci \
-    Tag
+#PRODUCT_PACKAGES += \
+#    nfc_nci.bcm2079x.default \
+#    android.hardware.nfc@1.0-impl \
+#    android.hardware.nfc@1.0-service \
+#    NfcNci \
+#    Tag
 
 # NFC feature files + configuration
-PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.nfc.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.nfc.xml \
-    frameworks/native/data/etc/android.hardware.nfc.hce.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.nfc.hce.xml \
-    device/lge/mako/nfc/libnfc-brcm.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-brcm.conf
+#PRODUCT_COPY_FILES += \
+#    frameworks/native/data/etc/android.hardware.nfc.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.nfc.xml \
+#    frameworks/native/data/etc/android.hardware.nfc.hce.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.nfc.hce.xml \
+#    device/lge/mako/nfc/libnfc-brcm.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-brcm.conf
 
 # Bluetooth HAL
 PRODUCT_PACKAGES += \
